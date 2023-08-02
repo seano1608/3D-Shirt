3e0d061d112bcec0674cbe1f68558d4c7708a611
Sean Thomson <seantomo1680@gmail.com>
2023-08-02 22:49:05 +0100

    fixed backendUrl

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://threed-ai-shirt-wjo1.onrender.com/",
+    backendUrl: "https://threed-ai-shirt-wjo1.onrender.com/api/v1/dalle",
   },
 };
 
